94a77ec61280e4fa87c6c2d5c1c9ef511d992d78
theprince <cfcrespinro@unitru.edu.pe>
2026-05-31 18:22:46 -0500

    chore: añadir soporte automático de compilación para Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === 'serve' ? '/' : '/wp-content/themes/educacion-primaria/dist/',
-  build: {
-    outDir: '../wp-content/themes/educacion-primaria/dist',
-    emptyOutDir: true,
-  }
-}))
+export default defineConfig(({ command }) => {
+  const isVercel = process.env.VERCEL === '1';
+
+  return {
+    plugins: [react()],
+    base: isVercel || command === 'serve' ? '/' : '/wp-content/themes/educacion-primaria/dist/',
+    build: {
+      outDir: isVercel ? 'dist' : '../wp-content/themes/educacion-primaria/dist',
+      emptyOutDir: true,
+    }
+  };
+})
