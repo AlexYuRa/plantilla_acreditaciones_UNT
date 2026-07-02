5453f8d82528dbe32a4413c23c260e2b03150f16
Alex Gianmarco Yupanqui Ramos <agyupanquira@unitru.edu.pe>
2026-07-02 13:27:52 -0500

    Fotos reales

--- profiles/escuela/config/branding.ts
+++ profiles/escuela/config/branding.ts
@@ -8,8 +8,8 @@ import logoEscuela from '../assets/logo-escuela.png';
 import logoBolsa from '../assets/logo-bolsa-trabajo.png';
 import libroReclamaciones from '../assets/libro-reclamaciones.svg';
 import hero1 from '../assets/hero/hero-1.webp';
-import hero2 from '../assets/hero/hero-2.webp';
-import hero3 from '../assets/hero/hero-3.webp';
+import hero2 from '../assets/hero/hero-2.jpg';
+import hero3 from '../assets/hero/hero-3.jpg';
 
 export const branding: BrandingConfig = {
   logos: {
